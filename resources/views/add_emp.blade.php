0e8ea373b75060209d2d87fddd2fed23c53bed05
Ghaidaa-kn <gh.kneish@gmail.com>
2023-02-11 14:10:08 +0200

    ''

--- resources/views/add_emp.blade.php
+++ resources/views/add_emp.blade.php
@@ -1,26 +1,26 @@
 @extends('master')
 @section('content')
 
-    <h1>Add New Employee</h1>
+    <h1 class="headers">Add New Employee</h1>
     <form action="/add_emp" method="POST">
       {{ csrf_field() }}
-      <h4>First Name :</h4>
+      <h4 class="headers">First Name :</h4>
       <div class="mb-3">
           <input type="text" name="first_name" class="form-control">
       </div>
-      <h4>Last Name :</h4>
+      <h4 class="headers">Last Name :</h4>
       <div class="mb-3">
           <input type="text" name="last_name" class="form-control">
       </div>
-      <h4>Email :</h4>
+      <h4 class="headers">Email :</h4>
       <div class="mb-3">
           <input type="text" name="email" class="form-control">
       </div>
-      <h4>Phone :</h4>
+      <h4 class="headers">Phone :</h4>
       <div class="mb-3">
           <input type="text" name="phone" class="form-control">
       </div>
-      <h4>Company :</h4>
+      <h4 class="headers">Company :</h4>
       <div class="mb-3">
         <select name="company" id="company" class="form-control" style="height: 35px;">
           @foreach($companies as $company)
